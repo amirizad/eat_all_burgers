56b85f737e3bdb1242b825c5bdb5bd9b19297d98
Amir Izadoanah <info@amirizad.com>
2017-06-25 16:50:21 -0700

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ use burgers_db;
 
 create table if not exists burgers(
 	id integer auto_increment not null,
-    burger_name varchar(150),
+    burger_name varchar(150) not null,
     devoured boolean not null default 0,
     createdAt timestamp not null,
     primary key (id)
